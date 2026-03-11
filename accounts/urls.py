6e60cb5511ddb6e43f6c20ba6534830b65bb7a85
M K Samuel Joseph <mksamjoseph29@gmail.com>
2026-03-11 15:38:36 +0530

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts.views import *
+
+urlpatterns = [
+    path('', landing, name='landing'),
+    path('ca-dashboard/', ca_dashboard, name='ca_dashboard'),
+    path('customer-dashboard/', customer_dashboard, name='customer_dashboard'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,39 @@
+from urllib import request
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import User
+# Create your views here.
+
+def landing(request):
+    #print("METHOD:", request.method)
+    #print("POST DATA:", request.POST)
+    if request.method == "POST":
+        email = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        print(email, password)
+        try:
+            user = authenticate(request, username=email, password=password)
+        except User.DoesNotExist:
+            user = None
+        print("Authenticated User:", user)
+        if user is not None:
+            login(request, user)
+            print("successfully logged in")
+            if user.role == "CA":
+                return redirect("/ca-dashboard/")
+            else:
+                return redirect("/customer-dashboard/")
+        else:
+            return render(request, "landing.html", {"error": "Invalid email or password"})
+
+    return render(request, "landing.html")
+
+
+def ca_dashboard(request):
+    return render(request, "ca_dashboard.html")
+
+
+def customer_dashboard(request):
+    return render(request, "customer_dashboard.html")
